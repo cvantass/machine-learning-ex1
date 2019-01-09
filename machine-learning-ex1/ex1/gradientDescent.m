dd8ba191ab3abf72452494edb9ac5258e13b866d
Charles Van Tassel <46469084+cvantass@users.noreply.github.com>
2019-01-09 13:41:53 -0500

    Update gradientDescent.m

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -9,23 +9,23 @@ J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
+% ====================== YOUR CODE HERE ======================
+% Instructions: Perform a single gradient step on the parameter vector
+%               theta. 
+%
+% Hint: While debugging, it can be useful to print out the values
+%       of the cost function (computeCost) and gradient here.
+%
 h = X * theta;
 error = h - y;
 theta_change = (alpha*(X' * error))/m;
 theta = theta - theta_change;
 
 
-    % ============================================================
+% ============================================================
 
-    % Save the cost J in every iteration    
-    J_history(iter) = computeCost(X, y, theta);
+% Save the cost J in every iteration    
+J_history(iter) = computeCost(X, y, theta);
 
 end
 
